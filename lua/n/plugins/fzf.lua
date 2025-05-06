cc7e53c0d5e8a4cbd174c4acbb7459e0e1ebb6be
n1kolas <n1kolas@users.noreply.github.com>
2025-05-06 14:15:57 +0200

    fzf_lua: search .gitignore files

--- lua/n/plugins/fzf.lua
+++ lua/n/plugins/fzf.lua
@@ -1,15 +1,19 @@
 return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-  },
-  config = function()
-    -- Search Files
-    vim.keymap.set("n", "<C-p>", require("fzf-lua").files, {desc = "Files"})
-    vim.keymap.set("n", "<leader>pf", require("fzf-lua").files, {desc = "Files"})
+	"ibhagwan/fzf-lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {},
+	config = function()
+		-- Search Files
+		vim.keymap.set("n", "<C-p>", function()
+			require("fzf-lua").files({ hidden = true, no_ignore = true })
+		end, { desc = "Files" })
+		vim.keymap.set("n", "<leader>pf", function()
+			require("fzf-lua").files({ hidden = true, no_ignore = true })
+		end, { desc = "Files" })
 
-    -- Search Contents
-    vim.keymap.set("n", "<leader>pg", require("fzf-lua").live_grep_native, {desc = "Grep"})
-
-  end
+		-- Search Contents
+		vim.keymap.set("n", "<leader>pg", function()
+			require("fzf-lua").live_grep_native({ hidden = true, no_ignore = true })
+		end, { desc = "Grep" })
+	end,
 }
